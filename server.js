d419c641244a1bc23ae1e94734812f2a7a315f2b
Julia Gustafsson <juliagustafsson96@hotmail.com>
2025-04-14 21:36:05 +0200

    Create base structure for database connection

--- server.js
+++ server.js
@@ -5,3 +5,20 @@ require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Connect to database
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.log("Connection failed: " + err);
+    }
+
+    console.log("Connected to database");
+});
+
